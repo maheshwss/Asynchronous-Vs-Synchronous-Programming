625bf926236ca11477cb145baf57d9297f76e40b
maheshwss <maheshy@wharfstreetstrategies.com>
2022-10-07 15:24:21 +0530

    async and sync programming

---
+++ main.js
@@ -0,0 +1,26 @@
+// Synchronous
+// let a = 1;
+// let b = 2;
+
+// console.log('Synchronous');
+
+// console.log(a);
+// console.log(b);
+
+
+// Asynchronous
+let a = 1;
+let b = 2;
+
+setTimeout(function() {
+    console.log('Async');
+}, 100);
+
+fetch('/').then(function() {
+    console.log('Fetch')
+});
+
+console.log('Synchronous');
+
+console.log(a);
+console.log(b);
